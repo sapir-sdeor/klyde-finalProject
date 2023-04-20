ce091b26a6e8882351e400c124370cca2554f63c
nitzankur <nitzan.knoll@mail.huji.ac.il>
2023-04-20 11:44:01 +0300

    navmeshlink in tunrime

--- Assets/Scripts/tryRotate/rotate2exis.cs
+++ Assets/Scripts/tryRotate/rotate2exis.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.AI.Navigation;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Experimental.AI;
 
 public class rotate2exis : MonoBehaviour
 {
@@ -11,6 +13,7 @@ public class rotate2exis : MonoBehaviour
 
     private bool _isRotating = false;
     private Vector3 _previousMousePos;
+    private List<GameObject> navlinks = new List<GameObject>(100);
     void Start()
     {
         for (int i = 0; i < surfaces.Length; i++) 
@@ -33,16 +36,52 @@ public class rotate2exis : MonoBehaviour
         if (Input.GetMouseButtonUp(0))
         {
             _isRotating = false;
+            // Find the closest point on the NavMesh to the first surface
+            // Find the closest point on the NavMesh to the first surface
+            Vector3 surface1Position =surfaces[0].transform.position; // Example position of the first surface
+            NavMeshHit surface1Hit;
+            bool foundSurface1Hit = NavMesh.SamplePosition(surface1Position, out surface1Hit, float.PositiveInfinity, NavMesh.AllAreas);
+            if (foundSurface1Hit)
+            {
+                surface1Position = surface1Hit.position;
+            }
+
+// Find the closest point on the NavMesh to the second surface
+            Vector3 surface2Position = surfaces[1].transform.position; // Example position of the second surface
+            NavMeshHit surface2Hit;
+            bool foundSurface2Hit = NavMesh.SamplePosition(surface2Position, out surface2Hit, float.PositiveInfinity, NavMesh.AllAreas);
+            if (foundSurface2Hit)
+            {
+                surface2Position = surface2Hit.position;
+            }
+            
+            // Create a new GameObject and add a NavMeshLink component
+            GameObject navLinkObject = new GameObject("NavLink");
+            NavMeshLink navLink = navLinkObject.AddComponent<NavMeshLink>();
+            navlinks.Add(navLinkObject);
+
+            // Set the start and end points of the navmesh link
+            navLink.startPoint = surface1Position;
+            navLink.endPoint =surface2Position;
+            
+            // Set the width and height of the navmesh link
+            navLink.width =2;
+
+            // Set the area type of the navmesh link
+            navLink.area = NavMesh.AllAreas;
             for (int i = 0; i < surfaces.Length; i++) 
             {
                 surfaces [i].BuildNavMesh ();    
-            }    
+            }   
         }
 
         // Rotate the object if the left mouse button is pressed down
         if (_isRotating)
         {
-            // Get the current mouse position
+            if (navlinks.Count >= 1 && navlinks[navlinks.Count-1] != null)
+            {
+                Destroy(navlinks[navlinks.Count-1]);
+            }
             Vector3 currentMousePos = Input.mousePosition;
 
             // Calculate the distance between the current and previous mouse positions
