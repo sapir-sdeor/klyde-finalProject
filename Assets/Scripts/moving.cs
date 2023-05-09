3d3b33b2a9aadb43e869be45af897b49ca19bcb1
nitzankur <nitzan.knoll@mail.huji.ac.il>
2023-05-09 10:31:46 +0300

    with try to match the resolution for every landscape screen

--- Assets/Scripts/moving.cs
+++ Assets/Scripts/moving.cs
@@ -29,33 +29,18 @@ public class moving : MonoBehaviour
            isWalk = true;
            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
            Debug.DrawRay(ray.origin, ray.direction * 50, Color.red);
-           if (Physics.Raycast(ray, out RaycastHit hit))
+           print(Physics.Raycast(ray, out RaycastHit raycast));
+           if (Physics.Raycast(ray, out RaycastHit raycastHit))
            {
-               print(Rotate2D3D.GetIsRotating());
+               // print(Rotate2D3D.GetIsRotating());
                if (agent.isOnNavMesh && !Rotate2D3D.GetIsRotating()) // Check if agent is on NavMesh
                {
-                   agent.SetDestination(hit.point);
+                   agent.SetDestination(raycastHit.point);
                }
            }
            else
            {
-               Vector3 mousePos = Input.mousePosition;
-               mousePos.z = Camera.main.nearClipPlane;
-           
-               // Convert the mouse position from screen space to world space
-               Vector3 mousePositionInWorldSpace = camera.ScreenToWorldPoint(mousePos);
-               // print(mousePositionInWorldSpace);
-
-               // Find the closest point on the NavMesh to the mouse position
-               NavMesh.SamplePosition(mousePositionInWorldSpace, out NavMeshHit hit, 1000f, NavMesh.AllAreas);
-
-               // Set the destination of the NavMeshAgent to the closest point on the NavMesh
-               print(hit.position);
-
-               if (agent.isOnNavMesh && !Rotate2D3D.GetIsRotating()) // Check if agent is on NavMesh
-               {
-                   agent.SetDestination(hit.position);
-               }
+               
            }
 
         }
---
+++ Assets/Scripts/tryRotate/tmp.cs
@@ -0,0 +1,23 @@
+namespace tryRotate
+{
+    public class tmp
+    {
+        // Vector3 mousePos = Input.mousePosition;
+        //                mousePos.z = Camera.main.nearClipPlane;
+        //            
+        //                // Convert the mouse position from screen space to world space
+        //                Vector3 mousePositionInWorldSpace = camera.ScreenToWorldPoint(mousePos);
+        //                // print(mousePositionInWorldSpace);
+        //
+        //                // Find the closest point on the NavMesh to the mouse position
+        //                NavMesh.SamplePosition(mousePositionInWorldSpace, out NavMeshHit hit, 1000f, NavMesh.AllAreas);
+        //
+        //                // Set the destination of the NavMeshAgent to the closest point on the NavMesh
+        //                print(hit.position);
+        //
+        //                if (agent.isOnNavMesh && !Rotate2D3D.GetIsRotating()) // Check if agent is on NavMesh
+        //                {
+        //                    agent.SetDestination(hit.position);
+        //                }
+    }
+}
