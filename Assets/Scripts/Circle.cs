147a2821e8906e3e3381f5178b10fc737422b41f
nitzankur <nitzan.knoll@mail.huji.ac.il>
2023-05-31 09:51:40 +0300

    fix door and klideon in circle

--- Assets/Scripts/Circle.cs
+++ Assets/Scripts/Circle.cs
@@ -20,19 +20,55 @@ public class Circle : World
     }
     
     // Update is called once per frame
+    // void Update()
+    // {
+    //     var trans = klyde.transform;
+    //     // var currAngle = trans.eulerAngles.y;
+    //     Vector3 direction = trans.position - Vector3.zero;
+    //     // Calculate the angle between the direction vector and the forward vector
+    //     float currAngle = Vector3.Angle(Vector3.forward, direction);
+    //     if (trans.position.x < 0) currAngle = 360 - currAngle;
+    //
+    //     isKlydeOn =(_angle * (halfNum - 1)  <= currAngle && currAngle <= _angle * halfNum) ;
+    //     var parentAngle = transform.localEulerAngles.y;
+    //     if (parentAngle < 0) parentAngle += 360;
+    //     if (parentAngle >= 360) parentAngle += 360 - parentAngle;
+    //     foreach (var child in GetComponentsInChildren<Transform>())
+    //     {
+    //         if (!child.gameObject.CompareTag("circle")) continue;
+    //         if (RecognizeShape.GetRecognizeShape() && !_changeTexture)
+    //         {
+    //             child.GetComponent<MeshRenderer>().material.mainTexture = textureWithoutShape;
+    //             _changeTexture = true;
+    //         }
+    //         // print(_angle + " " + halfNum);
+    //         child.GetComponent<MeshRenderer>().material.SetFloat("_Angle", _angle);
+    //         child.GetComponent<MeshRenderer>().material.SetInt("_HalfNum", halfNum);
+    //     }
+    // }
     void Update()
     {
         var trans = klyde.transform;
-        // var currAngle = trans.eulerAngles.y;
         Vector3 direction = trans.position - Vector3.zero;
         // Calculate the angle between the direction vector and the forward vector
-        float currAngle = Vector3.Angle(Vector3.forward, direction);
-        if (trans.position.x < 0) currAngle = 360 - currAngle;
+        float currAngle = Vector3.SignedAngle(Vector3.forward, direction, Vector3.up);
+        if (currAngle < 0) currAngle += 360f;
 
-        isKlydeOn =(_angle * (halfNum - 1)  <= currAngle && currAngle <= _angle * halfNum) ;
+        // Calculate the adjusted angle range based on the number of halves
+        float startAngle = _angle * (halfNum - 1);
+        float endAngle = _angle * halfNum;
+
+        // Handle wraparound from 360 to 0 degrees
+        if (endAngle > 360f)
+            endAngle -= 360f;
+
+        // Check if the current angle is within the adjusted range
+        isKlydeOn = IsAngleWithinRange(currAngle, startAngle, endAngle);
+
         var parentAngle = transform.localEulerAngles.y;
         if (parentAngle < 0) parentAngle += 360;
         if (parentAngle >= 360) parentAngle += 360 - parentAngle;
+
         foreach (var child in GetComponentsInChildren<Transform>())
         {
             if (!child.gameObject.CompareTag("circle")) continue;
@@ -41,10 +77,45 @@ public class Circle : World
                 child.GetComponent<MeshRenderer>().material.mainTexture = textureWithoutShape;
                 _changeTexture = true;
             }
-            // print(_angle + " " + halfNum);
             child.GetComponent<MeshRenderer>().material.SetFloat("_Angle", _angle);
             child.GetComponent<MeshRenderer>().material.SetInt("_HalfNum", halfNum);
         }
     }
+
+    
+    // Helper method to check if an angle is within a specified range
+    private bool IsAngleWithinRange(float angle, float start, float end)
+    {
+        // Normalize the angles to ensure proper comparison
+        angle = NormalizeAngle(angle);
+        start = NormalizeAngle(start);
+        end = NormalizeAngle(end);
+
+        // Check if the angle is within the specified range
+        if (start <= end)
+        {
+            return angle >= start && angle <= end;
+        }
+        else
+        {
+            return angle >= start || angle <= end;
+        }
+    }
+
+// Helper method to normalize an angle to the range of 0-360 degrees
+    private float NormalizeAngle(float angle)
+    {
+        if (angle < 0f)
+        {
+            angle %= 360f;
+            angle += 360f;
+        }
+        else if (angle >= 360f)
+        {
+            angle %= 360f;
+        }
+        return angle;
+    }
+
 }
   
