46ed62513df1e397b91505e015f3a873b420595a
sapirsdeor <sapir.sdeor@mail.huji.ac.il>
2023-06-14 11:10:42 +0300

    update level4 and canvas

---
+++ Assets/Scripts/ReflectPoint.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReflectPoint : MonoBehaviour
+{
+    [SerializeField] private int halfNum;
+    [SerializeField] private float offset;
+    private float _angle;
+    
+    private void Start()
+    {
+        _angle = 360 /(float) LevelManager.GetNumOfHalves();
+    }
+
+    private void Update()
+    {
+        var trans = transform;
+        Vector3 direction = trans.position - Vector3.zero;
+        // Calculate the angle between the direction vector and the forward vector
+        float currAngle = Vector3.SignedAngle(Vector3.forward, direction, Vector3.up);
+        if (currAngle < 0) currAngle += 360f;
+
+        // Calculate the adjusted angle range based on the number of halves
+        float startAngle = offset + _angle * (halfNum - 1);
+        float endAngle = offset + _angle * halfNum;
+
+        // Handle wraparound from 360 to 0 degrees
+        if (endAngle > 360f)
+            endAngle -= 360f;
+
+        // Check if the current angle is within the adjusted range
+        if (IsAngleWithinRange(currAngle, startAngle, endAngle))
+        {
+            EnabledChild(true);
+        }
+        else
+        {
+            EnabledChild(false);
+        }
+    }
+    
+    private bool IsAngleWithinRange(float angle, float start, float end)
+    {
+        // Normalize the angles to ensure proper comparison
+        angle = NormalizeAngle(angle);
+        start = NormalizeAngle(start);
+        end = NormalizeAngle(end);
+
+        // Check if the angle is within the specified range
+        if (start <= end)
+        {
+            return angle >= start && angle <= end;
+        }
+        return angle >= start || angle <= end;
+        
+    }
+    
+    private float NormalizeAngle(float angle)
+    {
+        if (angle < 0f)
+        {
+            angle %= 360f;
+            angle += 360f;
+        }
+        else if (angle >= 360f)
+        {
+            angle %= 360f;
+        }
+        return angle;
+    }
+    
+    private void EnabledChild(bool enabled)
+    {
+        foreach (var child in GetComponentsInChildren<MeshRenderer>())
+        {
+            child.GetComponent<MeshRenderer>().enabled = enabled;
+        }
+    }
+}
