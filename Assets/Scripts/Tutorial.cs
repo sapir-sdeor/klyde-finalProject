dbf5604a034549744f45364315544ea47c4dd2e7
nitzankur <nitzan.knoll@mail.huji.ac.il>
2023-06-25 18:28:12 +0300

    with level 1.5

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -10,14 +10,37 @@ public class Tutorial : World
 {
     [SerializeField] private GameObject walkHint;
     [SerializeField] private GameObject rotateHint;
+    private bool _rotateOnce,_rotateHintShow;
+
+    private void Start()
+    {
+        if (LevelManager.GetLevel() == 2)
+        {
+            rotateHint.SetActive(false);
+        }
+    }
 
 
     private void Update()
     {
         if (moving.GetIsWalk())
+        {
             walkHint.SetActive(false);
-        else if (Rotate2D3D.GetIsRotating())
+            if (LevelManager.GetLevel() == 2 && !_rotateOnce)
+            {
+                rotateHint.SetActive(true);
+                _rotateHintShow = true;
+            }
+        }
+            
+        else if (Rotate2D3D.GetIsRotating() && LevelManager.GetLevel() == 0)
             rotateHint.SetActive(false);
+
+        if (_rotateHintShow && Rotate2D3D.GetIsRotating())
+        {
+            rotateHint.SetActive(false);
+            _rotateOnce = true;
+        } 
     }
 }
   
