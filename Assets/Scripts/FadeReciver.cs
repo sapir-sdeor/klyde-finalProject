f7fb24611bdcdfa62d91c85fd2dcd59aadcabd9e
nitzankur <nitzan.knoll@mail.huji.ac.il>
2023-06-29 10:34:16 +0300

    fix the transission between scenes

---
+++ Assets/Scripts/FadeReciver.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeReciver : MonoBehaviour
+{
+    public void FadeOutComplete()
+    {
+        LevelManager.FadeOutComplete();
+    }
+}
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -7,16 +7,25 @@ using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
-    private static int _level = 6;
+    private static int _level = 2;
     private static int _numOfHalves = 0;
     private static LevelManager _instance;
-    private static GameObject panelPade;
+    private static GameObject _panelFade;
+    private static bool _fadeOutComplete;
     
     private void Awake()
     {
         SetNumOfHalves();
         _instance = this;
-        panelPade = GameObject.FindWithTag("fade");
+        _panelFade = GameObject.FindWithTag("fade");
+        RegisterFadeOutCallback();
+    }
+    
+    public static void Initialize(GameObject panelFade)
+    {
+        // _level = 0;
+        // _panelFade = panelFade;
+        
     }
 
     private void Update()
@@ -29,17 +38,59 @@ public class LevelManager : MonoBehaviour
         _level++;
         if (Levels.unlockedLevel == _level-1)
             Levels.unlockedLevel += 1;
-        panelPade.GetComponent<Animator>().SetTrigger("fadeOut");
-        _instance.StartCoroutine(WaitForLoadNextLevel());
+        _panelFade.GetComponent<Animator>().SetTrigger("fadeOut");
+        // _instance.StartCoroutine(WaitForLoadNextLevel());
         // SceneManager.LoadScene("Level5");
         //  _currentLevel = LevelFactory.CreateLevel(_level);
     }
-
-    static IEnumerator WaitForLoadNextLevel()
+    
+    private static void RegisterFadeOutCallback()
     {
-        yield return new WaitForSeconds(1.5f);
-        SceneManager.LoadScene("Level" + _level);
+        Animator fadeAnimator = _panelFade.GetComponent<Animator>();
+
+        // Register a callback for the fade-out animation's "OnComplete" event
+        fadeAnimator.GetCurrentAnimatorClipInfo(0);
+        AnimationClip[] clips = fadeAnimator.runtimeAnimatorController.animationClips;
+        foreach (AnimationClip clip in clips)
+        {
+            print("clip"+clip);
+            if (clip.name == "fade-out")
+            {
+                AnimationEvent animEvent = new AnimationEvent();
+                animEvent.functionName = "FadeOutComplete";
+                animEvent.time = clip.length;
+                print("animEvent.time"+animEvent.time);
+                clip.AddEvent(animEvent);
+                break;
+            }
+        }
     }
+
+    public static void FadeOutComplete()
+    {
+        _fadeOutComplete = true;
+        print("fadeOutComplete");
+        LoadNextLevel();
+    }
+
+    private static void LoadNextLevel()
+    {
+        if (_fadeOutComplete)
+        {
+            SceneManager.LoadScene("Level" + _level);
+        }
+    }
+
+    // static IEnumerator WaitForLoadNextLevel()
+    // {
+    //     yield return new WaitForSeconds(1.5f);
+    //     SceneManager.LoadScene("Level" + _level);
+    //     AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Level" + _level);
+    //     while (!asyncLoad.isDone)
+    //     {
+    //         yield return null;
+    //     }
+    // }
     
     public static void SetLevel(int newLevel)
     {
